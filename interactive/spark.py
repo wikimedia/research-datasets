66a6fc735fbb936245943401bb3f5f4a559c31ed
fab <fkaelin@wikimedia.org>
2021-03-09 10:18:56 -0500

    interactive folder for ipythoning

---
+++ interactive/spark.py
@@ -0,0 +1,39 @@
+# %%
+
+! pip install pillow
+! pip install python-swiftclient
+! pip install imageio
+! pip install xlsxWriter
+# !pip install --ignore-installed --upgrade git+https://github.com/wikimedia/wmfdata-python.git@release
+! pip install --ignore-installed --upgrade git+https://github.com/ottomata/wmfdata.git@conda
+
+#%%
+import imageio
+from io import BytesIO
+from PIL import Image
+
+
+from collections import Counter
+import datetime
+import itertools
+import math
+import mwparserfromhell
+import numpy as np
+import os
+import pandas as pd
+import re
+import wmfdata
+
+# %%
+spark = wmfdata.spark.get_session(app_name = 'coviding', ship_python_env=False)
+
+from pyspark.sql import Row, SparkSession, Window
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from operator import add
+
+# transform is available in spark 3, remove when switching versions
+from pyspark.sql.dataframe import DataFrame
+def transform(self, func):
+    return func(self)
+DataFrame.transform = transform
