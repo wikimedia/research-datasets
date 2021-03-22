5186ed3e5dfa05cce3cc6bdecd417674b510d0dd
fab <fkaelin@wikimedia.org>
2021-03-21 21:18:27 -0400

    one additional repartition shuffle to create a fixed number of files

--- images/swift.py
+++ images/swift.py
@@ -7,6 +7,7 @@ import base64
 import hashlib
 import imageio
 from io import BytesIO
+import math
 import mwparserfromhell
 import numpy as np
 import os
@@ -18,62 +19,13 @@ import wmfdata
 
 # %%
 
-# measurments from a test:
-mb_per_image = 61.8e3/1e6
-images_per_second_per_thread = 1e6/(4.6*60*60)/(20*4)
-# images on commons to download
-num_commons = 53e6
-
-#config
-num_images=num_commons
-# num_images=1e6
-file_size=250
-
-num_cores=80
-query_per_seconds=100
-
-total_mb=num_images*mb_per_image
-print(f'for {num_images} images, expected GB {num_images*mb_per_image/1e3} of image data')
-print(f'use {num_images*mb_per_image/file_size} partitions for files of size {file_size}mb')
-print(f'expected total duration with {num_cores} cores: {num_images/images_per_second_per_thread/3600/24/num_cores} days')
-# print(f'for a {query_per_seconds} qps to swift, use {query_per_seconds/images_per_second_per_thread} cores')
-# print(f'expected total duration: {num_images/query_per_seconds/3600}h ')
-
-# seconds_per_image = (4.6*60*60)/1e6
-# print(f'expected total duration for {num_images} images: {num_images*seconds_per_image/3600/24} days')
-
-
-#%%
-
-# measurments from a test:
-mb_per_image = 20*300/1e5
-images_per_second_executor = 1e5/(1.4*60*60)/10
-# images on commons to download
-num_commons = 53e6
-
-#config
-# num_images=num_commons
-num_images=1e6
-file_size=250
-
-num_cores=20
-query_per_seconds=100
-
-total_mb=num_images*mb_per_image
-print(f'for {num_images} images, expected GB {num_images*mb_per_image/1e3} of image data')
-print(f'use {num_images*mb_per_image/file_size} partitions for files of size {file_size}mb')
-print(f'expected total duration with {num_cores} cores: {num_images/images_per_second_executor/3600/24/num_cores} days')
-# print(f'for a {query_per_seconds} qps to swift, use {query_per_seconds/images_per_second_executor} executors')
-# print(f'expected total duration: {num_images/query_per_seconds/3600}h ')
-
-seconds_per_image = 34*60/1e5
-print(f'expected total duration for {num_images} images: {num_images*seconds_per_image/3600/24} days')
 
 #%%
 # the number of workers allocated to this spark application querying swift. 
 swift_load = 20
 
 spark_config = {
+    'spark.jars.packages': 'org.apache.spark:spark-avro_2.11:2.4.4',
     'spark.driver.extraJavaOptions' : ' '.join('-D{}={}'.format(k, v) for k, v in {
         'http.proxyHost': 'webproxy.eqiad.wmnet',
         'http.proxyPort': '8080',
@@ -85,32 +37,39 @@ spark_config = {
     'spark.executor.memory': '8g',
     'spark.executor.cores': 4,
     'spark.driver.memory': '4g',
-}   
+}
+
+# measurments from a test:
+mb_per_image = 61.8e3/1e6
+images_per_second_per_thread = 1e6/(4.6*60*60)/(20*4)
+# images on commons to download
+num_images_commons = 53e6
+
+def estimate_partitions(num_images, file_size_mb=200):
+    num_cores=4*swift_load
+    num_partitions=int(num_images*mb_per_image/file_size_mb)
+    total_mb=num_images*mb_per_image
+    print('estimate required number of partitions')
+    print(f'for {num_images} images, expected GB {num_images*mb_per_image/1e3} of image data')
+    print(f'use {num_partitions} partitions for files of size {file_size_mb}mb')
+    print(f'expected total duration with {num_cores} cores: {num_images/images_per_second_per_thread/3600/24/num_cores} days')
+    return num_partitions
+
 
 swift_host = "https://ms-fe.svc.eqiad.wmnet"
 max_retries = 5
 timeout =  15
-wiki_db = "commons"
-thumb_size = "400px"
 
 #%%
 
-
-swift_spark = wmfdata.spark.get_custom_session(
-    master="yarn",
-    app_name="swifting",
-    spark_config=spark_config)
+import findspark
+findspark.init('/usr/lib/spark2')
 
 from pyspark.sql import Row, SparkSession, Window
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 from operator import add
-
-
-#%%
-get_image_bytes('common','A_new_map_of_Upper_and_Lower_Canada_from_the_latest_authorities.jpg','400px')
-
-
+from pyspark import StorageLevel
 #%%
 
 @F.udf(returnType=T.BooleanType())
@@ -134,7 +93,7 @@ def get_image_bytes(wiki_db, file_name, thumb_size):
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def download_image(file_name):
+def download_image(file_name, wiki_db, thumb_size):
     #built in retries causes some dependency issue on the worker, doing retries manually
     error=None
     for retry_count in range(max_retries):
@@ -147,17 +106,18 @@ def download_image(file_name):
     return (None, error)
 
 # %%
-def download_images(input_df, partitions):
+def download_images(input_df, thumb_size, partitions):
     """
     `input_df` is expected to have a `image_file_name` column
     returns two dataframes
         `input` dataframe with a `image_bytes_b64` column containing the image bytes
         `input` dataframe with a `download_error` column containing the errors for each file that wasn't downloaded
     """
+    input_df = input_df.withColumn('thumbnail_size', F.lit(thumb_size))
+    input_df = input_df.repartition(partitions) if partitions is not None else input_df
     attempted_images_bytes = (input_df
-        .repartition(partitions)
-        .withColumn("attempted_thumbnail_b64", download_image('image_file_name'))
-        .cache())
+        .withColumn("attempted_thumbnail_b64", download_image('image_file_name', 'wiki_db', 'thumbnail_size'))
+        .persist(StorageLevel.DISK_ONLY))
 
     download_errors = (attempted_images_bytes
         .withColumn('download_error', F.col('attempted_thumbnail_b64').getItem(1))
@@ -173,6 +133,9 @@ def download_images(input_df, partitions):
 
 # %%
 
+#commons_hdfs_output_dir = 'commons_images/'
+#commons_download_errors_dir = 'commons_images_errors/'
+
 commons_cols = [
     "page_id",
     "page_namespace",
@@ -183,83 +146,72 @@ commons_cols = [
     "revision_text_sha1",
     "revision_timestamp",
     "snapshot",
-    "wiki_db"]
+    "wiki_db"] 
 
+def commons_images_to_hdfs(
+    images_hdfs_output,
+    snapshot,
+    wiki_db = "commons",
+    thumb_size='400px',
+    temp_wiki_dir='temp_wiki/',
+    desired_file_size_mb=200,
+    mb_per_swift_job=50000,
+    swift_download_errors=None):
+    """
+    downloads images from swift and extends the input
+    schema with the image bytes as base64 encoded strings
+    in avro encoded files
+    """
+    spark = wmfdata.spark.get_session(app_name = 'repartition wiki images', ship_python_env=False)
 
-# def commons_images_to_hdfs(
-#     wiki_db = "commons",
-#     snapshot = '2021-01',                   
-#     commons_hdfs_output_dir):
-wiki_db = "commons"
-snapshot = '2021-01'                   
-commons_hdfs_output_dir = 'commons_images/'
-commons_download_errors_dir = 'commons_images_errors/'
+    print(f'calculating the number of partitions required for a target file size {desired_file_size_mb}')
+    wiki = swift_spark.sql(f"select * from wmf.mediawiki_wikitext_current where snapshot='{snapshot}'")
+    wiki = wiki.where(F.col("wiki_db")==f'{wiki_db}wiki') if wiki_db is not None else wiki
+    wiki = wiki.filter(filter_images("page_title")).select(commons_cols)
 
+    wiki  = (wiki
+        .withColumn('image_file_name', image_file_name('page_title'))
+        #.limit(10))
+        .limit(100000))
+        #.limit0000))
+
+    # num_images = wiki.count()
+    num_images = 100000
+    num_partitions = estimate_partitions(num_images, desired_file_size_mb)
+
+    print(f"repartitioning the input wiki into {num_partitions} files for batched downloads")
+    (wiki
+        .repartition(num_partitions)
+        .write.format("avro").mode("overwrite").save(temp_wiki_dir))
+
+    print(f"downloading images from swift using {swift_load} executors")
+    files_per_batch=int(mb_per_swift_job/desired_file_size_mb)
+    print(f"using {math.ceil(num_partitions/files_per_batch)} spark jobs, processing around {files_per_batch} files ({mb_per_swift_job}MB) each")
+    for batch, file_range in enumerate([range(i,min(num_partitions, i + files_per_batch)) for i in range(0, num_partitions, files_per_batch)]):
+        paths = [f'{temp_wiki_dir}part-{f:05d}-*' for f in file_range ]
+        wiki_batch = swift_spark.read.format('avro').load(paths)
+        images, errors = download_images(input_df=wiki_batch, thumb_size=thumb_size, partitions=None)
+        print(f'downloading batch {batch} - files {file_range}')
+        (images
+           .withColumn('image_bytes_b64_sha1', F.sha1(F.col('image_bytes_b64')))
+           .write.format("avro").mode("overwrite").save(f'{images_hdfs_output}batch_{batch}')
+        )
+        if swift_download_errors is not None:
+            print(f"writing errors from swift")
+            errors.write.format("avro").mode("overwrite").save(f'{swift_download_errors}batch_{batch}')
+        print(f'finished batch {batch}')
+    print("all done")
 # %%
-wiki = (swift_spark
-    .sql(f"""
-    select * from wmf.mediawiki_wikitext_current where snapshot='{snapshot}' and wiki_db='{wiki_db}wiki'
-    """))
-
-
-wiki = wiki.filter(filter_images("page_title")).select(commons_cols)
-
+# sn='2021-01'
+# swift_spark.sql(f"select * from wmf.mediawiki_wikitext_current where snapshot='{sn}'").show()
+commons_images_to_hdfs(images_hdfs_output='test_images/',snapshot='2021-02',thumb_size='400px',desired_file_size_mb=200,mb_per_swift_job=25,swift_download_errors='test_errors/')
 # %%
 
 
-# commons_images_schema = (T.StructType()
-#     .add("page_id", T.StringType(), True)
-#     .add("page_namespace", T.StringType(), True)
-#     .add("page_title", T.StringType(), True)
-#     .add("user_id", T.StringType(), True)
-#     .add("user_text", T.StringType(), True)
-#     .add("revision_id", T.StringType(), True)
-#     .add("revision_text_sha1", T.StringType(), True)
-#     .add("revision_timestamp", T.StringType(), True)
-#     .add("snapshot", T.StringType(), True)
-#     .add("wiki_db", T.StringType(), True))
-
-# image_bytes_schema = (T.StructType()
-#     .add("image_file_name", T.StringType(), True)
-#     .add("image_bytes_b64_sha1", T.StringType(), True)
-#     .add("image_bytes_b64", T.StringType(), True))
-
-
-# %%
-
-test = (wiki
-    .withColumn('image_file_name', image_file_name('page_title'))
-    # .limit(10))
-    # .limit(100000))
-    .limit(1000000))
-# T.StructType(commons_images_schema.fields+image_bytes_schema.fields)
-
-# %%
-# test.show()
-images,errors = download_images(test,240)
-
+images = swift_spark.read.format('avro').load('test_images/*').show()
 #%%
-images.count()
-# errors.count()
-
-#%%
-(images
-    .withColumn('image_bytes_b64_sha1', F.sha1(F.col('image_bytes_b64')))
-    .write.mode("overwrite").save(commons_hdfs_output_dir)
-)
-
-
-(errors
-    .write.mode("overwrite").save(commons_download_errors_dir)
-)
-
-# %%
-# images.count()
-
-images = spark.read.parquet(commons_hdfs_output_dir)
-#%%
-errors = spark.read.parquet(commons_download_errors_dir)
-
+errors = swift_spark.read.format('avro').load('test_errors/*')
+errors.select('download_error').limit(10).collect()
 #%%
 e = errors.select('image_file_name', 'download_error').limit(200).collect()
 #%%
