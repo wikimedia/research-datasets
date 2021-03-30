58bff8cd374b1d1e63a7ec6e3b701dadb1b6ebda
fab <fkaelin@wikimedia.org>
2021-03-30 15:47:30 +0000

    add pyspark imports to images script

--- interactive/images.py
+++ interactive/images.py
@@ -14,3 +14,8 @@ import wmfdata
 # TODO, when yarn labels are available, add the label configuration to 
 # get a context for the GPU nodes only
 spark = wmfdata.spark.get_session(app_name = 'imageing', ship_python_env=True)
+
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import Row, SparkSession, Window
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
