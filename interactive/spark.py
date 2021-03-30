d051ce251a24b0a5153350e20edb046de8853b20
fab <fkaelin@wikimedia.org>
2021-03-29 23:35:25 -0400

    interactive spark

--- interactive/spark.py
+++ interactive/spark.py
@@ -1,36 +1,27 @@
-# %%
-
-! pip install pillow
-! pip install python-swiftclient
-! pip install imageio
-! pip install xlsxWriter
-# !pip install --ignore-installed --upgrade git+https://github.com/wikimedia/wmfdata-python.git@release
-! pip install --ignore-installed --upgrade git+https://github.com/ottomata/wmfdata.git@conda
-
-#%%
-import imageio
-from io import BytesIO
-from PIL import Image
-
-
-from collections import Counter
-import datetime
-import itertools
-import math
-import mwparserfromhell
-import numpy as np
-import os
-import pandas as pd
-import re
 import wmfdata
 
-# %%
-spark = wmfdata.spark.get_session(app_name = 'coviding', ship_python_env=False)
+spark = None
 
+create_spark = True
+
+if create_spark:
+    spark = wmfdata.spark.get_session(
+        app_name='interactive',
+        extra_settings={
+            'spark.jars.packages': 'org.apache.spark:spark-avro_2.11:2.4.4',
+            'spark.sql.shuffle.partitions': 512},
+        ship_python_env=False)
+else:
+    # Even if we don't want a running context at first, we still want
+    # to import the spark modules, so we run findspark manually
+    import findspark
+    findspark.init('/usr/lib/spark2')
+
+from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import Row, SparkSession, Window
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
-from operator import add
+
 
 # transform is available in spark 3, remove when switching versions
 from pyspark.sql.dataframe import DataFrame
